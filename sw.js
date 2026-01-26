b9c1a0d8e026e47ecef276476dc7330011d3719e
sparrowfarm613 <tina@sparrowfarms.ca>
2026-01-26 01:21:23 -0500

    Create Service Worker for app usage

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'sparrow-farms-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
